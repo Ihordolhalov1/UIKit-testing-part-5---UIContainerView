53bb28e5308f9e43b7524f3644bca066147917df
Ihordolhalov1 <servicegambro@gmail.com>
2022-09-07 16:04:42 +0100

    first commit

---
+++ UIKit testing part 5 - UIContainerView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UIKit testing part 5 - UIContainerView
+//
+//  Created by Ihor Dolhalov on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageCover: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
